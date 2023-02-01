e4ebe9591a8320793ed48d36a1862bf2e66c515e
Ritunjai Choudhary <ritunjai.choudhary@ad.infosys.com>
2023-02-01 15:36:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import axios from "axios"
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [userData, setuserData] = useState([])
+
+  useEffect(() => {
+    getUserData()
+  }, [])
+
+
+  const getUserData = () => {
+    axios.get('https://my-json-server.typicode.com/Ritunjai/dataset/userdata')
+      .then(response => {
+        console.log(response, "responseeee")
+        setuserData(response?.data)
+      })
+      .catch(error => {
+        console.log("error", error)
+      })
+  }
+
+  function calculateRewards(price) {
+    if (price >=50 && price < 100) {
+        return price-50;
+    } else if (price >100){
+        return (2*(price-100) + 50);
+    }
+    return 0;
+}
+
+  return (
+    <div className="App">
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Amount $</th>
+          <th>Rewards</th>
+        </tr>
+        {userData?.length > 0 ? userData.map(item => <tr>
+          <td>{item.name}</td>
+          <td>{item.amount}</td>
+          <td>{calculateRewards(item.amount)}</td>
+        </tr>) : 
+        <>     Loading...   </>
+        }
+      </table>
+    </div>
+  );
+}
+
+export default App;
